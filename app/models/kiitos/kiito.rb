e34c273e734fa32a02a45d35386e87c680df28f9
Eduardo Bautista & Ivan Calderon <pair+eduardo+ivan@crowdint.com>
2013-09-06 16:15:03 -0500

    Fix pg gem error which prevented the server from starting * Add pg gem in kiitos Gemfile * Add seed data * Add administrator - user association

--- app/models/kiitos/kiito.rb
+++ app/models/kiitos/kiito.rb
@@ -1,6 +1,6 @@
 module Kiitos
   class Kiito < ActiveRecord::Base
-    belongs_to :kiitos_category
+    belongs_to :kiitos_category, class_name: '::Kiitos::Category'
     validates :title, :kiitos_category_id, :image, presence: true
   end
 end
