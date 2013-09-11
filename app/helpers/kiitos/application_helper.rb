ab7c258b303b260778fd789e4e836b00414bb210
Eduardo Bautista & Ivan Calderon <pair+eduardo+ivan@crowdint.com>
2013-09-11 15:58:08 -0500

    Move view logic to helper

--- app/helpers/kiitos/application_helper.rb
+++ app/helpers/kiitos/application_helper.rb
@@ -1,4 +1,7 @@
 module Kiitos
   module ApplicationHelper
+    def all_users_but_current
+      ::Kiitos.user_class.all.map { |u| [u.email, u.id] unless u.email == current_user.email }.compact
+    end
   end
 end
