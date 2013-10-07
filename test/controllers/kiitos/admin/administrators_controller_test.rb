9f93d90245bb5fe7569e024957691d344ac844b1
Eduardo Bautista & Heriberto Perez <pair+eduardo+heriberto@crowdint.com>
2013-10-07 10:44:09 -0500

    Change test to test for the user id

--- test/controllers/kiitos/admin/administrators_controller_test.rb
+++ test/controllers/kiitos/admin/administrators_controller_test.rb
@@ -12,11 +12,11 @@ describe Kiitos::Admin::AdministratorsController  do
 
     it 'turns a user into an admin' do
       Kiitos::Administrator.all.count.must_equal 0
-      post :create, user_id: @user.id
-      Kiitos::Administrator.all.count.must_equal 1
+      post :create, id: @user.id
+      Kiitos::Administrator.last.user_id.must_equal @user.id
     end
     it 'redirects to the users management panel' do
-      post :create, user_id: 1
+      post :create, id: 1
       response.must_redirect_to admin_users_path
     end
   end
