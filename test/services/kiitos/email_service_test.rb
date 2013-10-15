d91c00cbaf1aa3f82538627277404899df3e2e83
Heriberto Perez <heriberto.perez@crowdint.com>
2013-10-15 11:40:35 -0500

    Create EmailService creation to let Signed-off-by: Eduardo Bautista <eduardo.bautista@crowdint.com>

--- test/services/kiitos/email_service_test.rb
+++ test/services/kiitos/email_service_test.rb
@@ -1,17 +1,12 @@
 require 'test_helper'
 
 describe Kiitos::EmailService do
-
-  before do
-    message = 'some message'
-    receiver = 'email@example.com'
-    @email_service = Kiitos::EmailService.new receiver, message
-  end
+  let(:email_service) { Kiitos::EmailService.new 'test@example.com', 'Test Message' }
 
   describe '#send_notification' do
     it 'should sends an email' do
       ActionMailer::Base.deliveries = []
-      @email_service.send_notification
+      email_service.send_notification
       ActionMailer::Base.deliveries.count.must_equal 1
     end
   end
