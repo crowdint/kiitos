78f259ea1f6ccd847c4c16c7f89f88136a5a2990
Jonathan Tapia <jonathan.tapia@crowdint.com>
2013-11-14 12:48:05 -0600

    Refactor of users index test

--- test/controllers/kiitos/users_controller_test.rb
+++ test/controllers/kiitos/users_controller_test.rb
@@ -1,19 +1,22 @@
 require 'test_helper'
 
 describe Kiitos::UsersController do
+  before do
+    @routes = Kiitos::Engine.routes
+  end
+
   describe 'GET :index' do
     before do
       User.create name: 'Test1 Name', email: 'test1@example.com'
       User.create name: 'Test2 Name', email: 'test2@example.com'
       User.create name: 'Test3 Name', email: 'test3@example.com'
-      User.create name: 'Test4 Name', email: 'test4@example.com'
-      @routes = Kiitos::Engine.routes
-      get :index
     end
 
     it 'returns JSON of existing users except kiitos_current_user' do
+      get :index
       json = JSON.parse(response.body)
-      json.length.must_equal 3
+      json.wont_include "{ id: 1, name: 'Test1 Name', email: 'test1@example.com' }"
+      json.length.must_equal 2
     end
   end
 end
